4629c0c48bfcff784c1285fab98917c2abf1492f
Ashish-Gupta7 <ash300mr@gmail.com>
2024-09-12 19:12:35 +0530

    omegle and razorpay seperated

--- 17webrtc/Omegle/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.ejs", "./public/javascripts/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
